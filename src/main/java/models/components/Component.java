9d6d45d3652eaf9e2a70fe706c26b77986952c06
vyvy921 <vyvy.tester@gmail.com>
2024-11-20 23:55:57 +0700

    lesson 6

--- src/main/java/models/components/Component.java
+++ src/main/java/models/components/Component.java
@@ -1,22 +1,39 @@
 package models.components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Component {
     protected WebDriver driver;
-    protected WebElement element;
+    protected WebElement component;
     protected WebDriverWait wait;
 
-    public Component(WebDriver driver, WebElement element) {
+    public Component(WebDriver driver, WebElement component {
         this.driver = driver;
-        this.element = element;
+        this.component = component;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
+    //narrow down searching
+    public WebElement findElement(By by) {
+        return component.findElement(by);
+    }
 
+    public List<WebElement> findElements(By by) {
+        return component.findElements(by);
+    }
+
+    public <T extends Component> T findComponent(Class<T> classComponent, WebDriver driver) {
+        return findComponents(classComponent, driver).get(0);
+    }
+
+    public <T> List<T> findComponents(Class<T> classComponent, WebDriver driver) {
+        return null;
+    }
 
 }
