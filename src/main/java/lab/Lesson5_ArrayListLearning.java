d4dcae6e7154a7486e5c0d13906737e2067a3a7c
vyvothiminh <vy.vothiminh@elca.vn>
2025-01-03 22:01:43 +0700

    lab day 4

--- src/main/java/lab/Lesson5_ArrayListLearning.java
+++ src/main/java/lab/Lesson5_ArrayListLearning.java
@@ -54,5 +54,61 @@ public class Lesson5_ArrayListLearning {
         System.out.println(myArrList);
 
         System.out.println(myArrList.indexOf(2));
+
+        get3MinValue();
     }
+
+    public static void get3MinValue(){
+        /*
+        * arrange from min -> max
+        * filter duplicated values
+        * get 3 firsts values
+        * */
+        List<Integer> myArr = new ArrayList<>();
+        myArr.add(3);
+        myArr.add(1);
+        myArr.add(6);
+        myArr.add(4);
+        myArr.add(6);
+        myArr.add(1);
+        myArr.add(2);
+        myArr.add(1);
+        myArr.add(4);
+
+        System.out.println("Initialization: ");
+        System.out.println(myArr);
+
+        System.out.println("After arranging: ");
+        int min, tmp;
+        for (int indexSorted = 0; indexSorted < myArr.size(); indexSorted++) {
+            for (int indexUnsorted = indexSorted + 1; indexUnsorted < myArr.size(); indexUnsorted++) {
+                if (myArr.get(indexUnsorted) < myArr.get(indexSorted)){
+                    min = myArr.get(indexUnsorted);
+                    tmp = myArr.get(indexSorted);
+                    myArr.set(indexSorted, min);
+                    myArr.set(indexUnsorted, tmp);
+                }
+            }
+
+        }
+        System.out.println(myArr);
+
+        System.out.println("After filter");
+        List<Integer> filteredList = new ArrayList<>();
+        filteredList.addAll(myArr);
+        System.out.println(filteredList);
+
+        for (int index = 0; index < filteredList.size(); index++) {
+            for (int pointer = index + 1; pointer < filteredList.size(); pointer++) {
+                if (filteredList.get(index).equals(filteredList.get(pointer))) {
+                    filteredList.remove(index + 1);
+                    pointer--;
+                }
+            }
+        }
+
+
+        System.out.println(filteredList);
+    }
+
 }
